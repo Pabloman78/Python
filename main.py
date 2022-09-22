fb7f12ffea318c804d4215b1443fdb6f8f13722a
Pabloman78 <oaocrcryr80@gmail.com>
2022-09-22 16:46:24 +0600

    fourth commit

--- main.py
+++ main.py
@@ -8,11 +8,15 @@
 # r.close()
 # r.close()
 
-name = open('names.txt', 'w', encoding='utf-8')
-name.write("Mamatkadyrov Artur")
-name.write("Маматкадыров Артур")
-name.close()
+# name = open('names.txt', 'w', encoding='utf-8')
+# name.write("Mamatkadyrov Artur")
+# name.write("Маматкадыров Артур")
+# name.close()
 
-read_name = open('names.txt', 'r', encoding='utf-8')
-print(read_name.read())
-read_name.close()
+# read_name = open('names.txt', 'r', encoding='utf-8')
+# print(read_name.read())
+# read_name.close()
+
+password = open('passwords.txt', 'a+')
+password.write('2021itrun\n')
+password.close()
