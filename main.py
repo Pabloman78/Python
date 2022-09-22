5893cae61eea48c1a130f2b91ae3ac14b17cfdfb
Pabloman78 <oaocrcryr80@gmail.com>
2022-09-22 16:51:59 +0600

    fifth commit

--- main.py
+++ main.py
@@ -17,6 +17,10 @@
 # print(read_name.read())
 # read_name.close()
 
-password = open('passwords.txt', 'a+')
-password.write('2021itrun\n')
-password.close()
+# password = open('passwords.txt', 'a+')
+# password.write('2021itrun\n')
+# password.close()
+
+py = open('hello.py', 'w')
+py.write('print("Hello world")')
+py.close()
